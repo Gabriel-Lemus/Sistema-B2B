268e85984cae196dfa3e9cf31fdda452c943ff86
Gabriel Lemus <glemus.stuart@gmail.com>
2022-02-26 23:41:49 -0600

    Add class attributes and constructor

--- server/sales-system/src/main/java/SqlTableCrud.java
+++ server/sales-system/src/main/java/SqlTableCrud.java
@@ -1,3 +1,36 @@
 public class SqlTableCrud {
-    
+    // Attributes
+    private String conUrl;
+    private String user;
+    private String password;
+    private String localhostIp;
+    private String servletUrl;
+    private String schema;
+    private String tableName;
+    private String primaryKey;
+    private String[] attributes;
+    private String[] types;
+    private boolean[] nullableAttributes;
+    private int maxRows;
+
+    // Constructor
+    public SqlTableCrud(String conUrl, String user, String password,
+            String localhostIp, String servletUrl, String schema,
+            String tableName, String primaryKey, String[] attributes,
+            String[] types, boolean[] nullableAttributes, int maxRows) {
+        super();
+
+        this.conUrl = conUrl;
+        this.user = user;
+        this.password = password;
+        this.localhostIp = localhostIp;
+        this.servletUrl = servletUrl;
+        this.schema = schema;
+        this.tableName = tableName;
+        this.primaryKey = primaryKey;
+        this.attributes = attributes;
+        this.types = types;
+        this.nullableAttributes = nullableAttributes;
+        this.maxRows = maxRows;
+    }
 }
