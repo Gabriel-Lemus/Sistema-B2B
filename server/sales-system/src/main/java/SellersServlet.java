6002e8ca8ce0ec626ac257de65c986be50f79dc1
Gabriel Lemus <glemus.stuart@gmail.com>
2022-03-07 00:12:30 -0600

    Add attributes and init method

--- server/sales-system/src/main/java/SellersServlet.java
+++ server/sales-system/src/main/java/SellersServlet.java
@@ -1,7 +1,28 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-
 @WebServlet("/sellers")
 public class SellersServlet extends HttpServlet {
+    // Attributes
+    private SqlSchema sqlSchema;
+    private ServletHelper helper;
+    private String connectionUrl;
+    private String user;
+    private String password;
+    private String adminConUrl;
+    private String adminUser;
+    private String adminPassword;
 
+    // Servlet initialization
+    public void init() throws ServletException {
+        helper = new ServletHelper();
+
+        // Standard connection
+        connectionUrl = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+
+        // Admin connection
+        adminConUrl = connectionUrl;
+        adminUser = "SYS as SYSDBA";
+        adminPassword = "Oracle18c";
+    }
 }
