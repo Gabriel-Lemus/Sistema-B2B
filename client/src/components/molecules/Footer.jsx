5c978adbb5c6a0afbfa296feceea6abc7a652f9f
Gabriel Lemus <glemus.stuart@gmail.com>
2022-03-12 04:47:22 -0600

    Add footer molecule

---
+++ client/src/components/molecules/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FooterContainer from './FooterContainer';
+
+function Footer() {
+  return (
+    <footer className="text-muted page-footer">
+      <FooterContainer />
+    </footer>
+  );
+}
+
+export default Footer;
