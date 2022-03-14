72914fd5e85a2fd3c512e5e2c6113dd386758c16
Gabriel Lemus <glemus.stuart@gmail.com>
2022-03-13 18:04:36 -0600

    Update component to display the images

--- client/src/components/molecules/DevicesCards.jsx
+++ client/src/components/molecules/DevicesCards.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 function DevicesCatalog(props) {
   return (
     <section className="devices-catalog">
-      {props.devices.map((device) => (
+      {props.devices.map((device, index) => (
         <div className="card product-card" key={props.devices.indexOf(device)}>
           <div className="card-body">
             <h5 className="card-title">{device.dispositivo}</h5>
             <img
               className="product-image"
               src={
-                device.imagen !== null && device.imagen !== undefined
-                  ? device.imagen
+                props.images[index] !== null && props.images[index] !== undefined
+                  ? `data:image/png;base64,${props.images[index]}`
                   : 'https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101028/112815904-no-image-available-icon-flat-vector-illustration.jpg?ver=6'
               }
               alt="Imagen del dispositivo"
@@ -22,7 +22,6 @@ function DevicesCatalog(props) {
               <br />
               {device.precio}
             </p>
-            {/* datos-dispositivo/electronica/3 */}
             <Link to={`/datos-dispositivo/${device.vendedor}/${device.id_dispositivo}`}>
               <button className="btn btn-primary">Ver más</button>
             </Link>
