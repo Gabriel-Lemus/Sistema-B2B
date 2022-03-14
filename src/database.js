683c50e581e82c0b774259881b18e205df193938
Dylan <rodas171315@unis.edu.gt>
2022-03-14 04:06:57 -0600

    Creando las vistas y reparando conexion a BD

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/db-factory', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
+    //useCreateIndex: true,
+    useNewUrlParser: true
+    //useFindAndModify: false
 })
     .then(db => console.log('DB is connected'))
     .catch(err => console.log(err));
