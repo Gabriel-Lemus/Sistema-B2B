aae688b6873d6e043f0f9593fd6b2614cb1f2f86
Gabriel Lemus <glemus.stuart@gmail.com>
2022-03-14 08:03:21 -0600

    Correct image check

--- client/src/components/pages/DevicesCatalog.jsx
+++ client/src/components/pages/DevicesCatalog.jsx
@@ -28,7 +28,7 @@ function DevicesCatalog() {
           let deviceData = await axios.get(
             `http://localhost:8080/sales-system/sellers?dispositivos=true&dispositivo=${devices[i].id_dispositivo}&vendedor=${devices[i].vendedor}`
           );
-          if (deviceData.data.success) {
+          if (deviceData.data.data[0].fotos) {
             images.push(deviceData.data.data[0].fotos[0]);
           } else {
             images.push(null);
