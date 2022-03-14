01f435305c3635b9c21d64001dc0983354554858
Dylan <rodas171315@unis.edu.gt>
2022-03-14 11:03:58 -0600

    Se incluyo el SignUp o Registro para usuarios

--- src/models/Device.js
+++ src/models/Device.js
@@ -3,7 +3,7 @@ const {Schema} = mongoose;
 
 const DeviceSchema = new Schema({
     title: {type: String, required: true},
-    description: {type: String, required: true},
+    description: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Device', DeviceSchema);
