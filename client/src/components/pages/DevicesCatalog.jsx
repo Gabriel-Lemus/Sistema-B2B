34b2ea33705d6869bb5b903ace749128a44e8200
Gabriel Lemus <glemus.stuart@gmail.com>
2022-03-21 15:24:56 -0600

    Fetch images from url and add loader

--- client/src/components/pages/DevicesCatalog.jsx
+++ client/src/components/pages/DevicesCatalog.jsx
@@ -3,66 +3,67 @@ import React, { useState, useEffect } from 'react';
 import helpers from '../../helpers/helpers';
 import DashboardTemplate from '../templates/DashboardTemplate';
 import DevicesCards from '../molecules/DevicesCards';
+import { css } from '@emotion/react';
+import DotLoader from 'react-spinners/DotLoader';
+
+const override = css`
+  position: absolute;
+  top: 35%;
+  left: 45%;
+  margin-top: -50px;
+  margin-left: -50px;
+`;
 
 function DevicesCatalog() {
   // State
   const [devices, setDevices] = useState([]);
-  const [devicesImages, setdevicesImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // Effects
   useEffect(() => {
     (async () => {
       let devicesData = await axios.get(
-        `http://${helpers.LOCALHOST_IP}:${helpers.TOMCAT_PORT}/sales-system/sellers?dispositivos=true&page=1`
+        `http://${helpers.LOCALHOST_IP}:${helpers.TOMCAT_PORT}/sales-system/sellers?get=true&dispositivos=true`
       );
-      setDevices(devicesData.data.data);
+      setDevices(devicesData.data.dispositivos);
+      setLoading(false);
     })();
   }, []);
 
-  useEffect(() => {
-    (async () => {
-      if (devices.length !== 0) {
-        let images = [];
-
-        for (let i = 0; i < devices.length; i++) {
-          let deviceData = await axios.get(
-            `http://localhost:8080/sales-system/sellers?dispositivos=true&dispositivo=${devices[i].id_dispositivo}&vendedor=${devices[i].vendedor}`
-          );
-          if (
-            Object.prototype.hasOwnProperty.call(
-              deviceData.data.data[0],
-              'fotos'
-            )
-          ) {
-            images.push(deviceData.data.data[0].fotos[0]);
-          } else {
-            images.push(null);
-          }
-        }
-
-        setdevicesImages(images);
-      }
-    })();
-  }, [devices]);
-
   return (
-    <DashboardTemplate
-      displaySearchBar={true}
-      sideBarItems={helpers.CLIENT_PAGES}
-      pageTitle="Catálogo de dispositivos"
-    >
-      {devices.length === 0 ? (
-        <>
-          <div className="alert alert-info mt-5" role="alert">
-            Lo sentimos, no hay dispositivos registrados en el sistema.
-          </div>
-        </>
+    <>
+      <DashboardTemplate
+        displaySearchBar={true}
+        sideBarItems={helpers.CLIENT_PAGES}
+        pageTitle="Catálogo de dispositivos"
+      >
+        {devices.length === 0 ? (
+          !loading ? (
+            <div className="alert alert-info mt-5" role="alert">
+              Lo sentimos, no hay dispositivos registrados en el sistema.
+            </div>
+          ) : (
+            <></>
+          )
+        ) : (
+          <>
+            <DevicesCards devices={devices} />
+          </>
+        )}
+      </DashboardTemplate>
+      {loading ? (
+        <div className="background-div">
+          <DotLoader
+            color={helpers.PALETTE.blue}
+            loading={loading}
+            css={override}
+            size={275}
+          />
+        </div>
       ) : (
-        <>
-          <DevicesCards devices={devices} images={devicesImages} />
-        </>
+        <></>
       )}
-    </DashboardTemplate>
+    </>
   );
 }
 
