9bda68095ec8ae988db7e0cd733ff5e06ed01bac
Gabriel Lemus <glemus.stuart@gmail.com>
2022-03-24 00:18:48 -0600

    Initial mail implementation

--- server/sales-system/src/main/java/Mailer.java
+++ server/sales-system/src/main/java/Mailer.java
@@ -1,6 +1,119 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @WebServlet("/mail")
 public class Mailer extends HttpServlet {
+    // Attributes
+
+    // Servlet initialization
+    public void init() throws ServletException {
+    }
+
+    // ========================= CRUD Methods =========================
+    /**
+     * Method to allow the handling of the post request to the schema instance.
+     * 
+     * @param request  The request to be handled.
+     * @param response The response to be handled.
+     * @throws ServletException If the request could not be handled.
+     * @throws IOException      If the request could not be handled.
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.print("{\"success\":" + false + ",\"message\":\"" + "Method not yet implemented." + "\"}");
+    }
+
+    /**
+     * Method to allow the handling of the get request to the schema instance.
+     * 
+     * @param request  The request to be handled.
+     * @param response The response to be handled.
+     * @throws ServletException If the request could not be handled.
+     * @throws IOException      If the request could not be handled.
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("glemus.luigi@gmail.com", "********");
+            }
+        });
+        session.setDebug(true);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("glemus.luigi@gmail.com"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress("glemus.stuart@gmail.com"));
+            message.setSubject("Test");
+            message.setText("This is a test");
+            Transport.send(message);
+            out.print("{\"success\":" + true + ",\"message\":\"" + "Email sent." + "\"}");
+        } catch (MessagingException e) {
+            out.print("{\"success\":" + false + ",\"message\":\"" + "Email not sent." + "\"}");
+        }
+    }
+
+    /**
+     * Method to allow the handling of the put request to the schema instance.
+     * 
+     * @param request  The request to be handled.
+     * @param response The response to be handled.
+     * @throws ServletException If the request could not be handled.
+     * @throws IOException      If the request could not be handled.
+     */
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.print("{\"success\":" + false + ",\"message\":\"" + "Method not yet implemented." + "\"}");
+    }
+
+    /**
+     * Method to allow the handling of the delete request to the schema instance.
+     * 
+     * @param request  The request to be handled.
+     * @param response The response to be handled.
+     * @throws ServletException If the request could not be handled.
+     * @throws IOException      If the request could not be handled.
+     */
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.print("{\"success\":" + false + ",\"message\":\"" + "Method not yet implemented." + "\"}");
+    }
 }
