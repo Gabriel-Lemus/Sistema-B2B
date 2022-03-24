c46b029f86a4509f30844033dcb3203874412c17
Gabriel Lemus <glemus.stuart@gmail.com>
2022-03-23 22:34:17 -0600

    Add loader

--- client/src/components/pages/ShoppingCart.jsx
+++ client/src/components/pages/ShoppingCart.jsx
@@ -1,22 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import DashboardTemplate from '../templates/DashboardTemplate';
 import helpers from '../../helpers/helpers';
 import ShoppingCartForm from '../organisms/ShoppingCartForm';
+import Loader from '../molecules/Loader';
 
 function ShoppingCart() {
+  // State
+  const [loading, setLoading] = useState(true);
+
   // Effects
   useEffect(() => {
     document.title = 'Carrito de Compras';
   }, []);
 
   return (
-    <DashboardTemplate
-      displaySearchBar={false}
-      sideBarItems={helpers.CLIENT_PAGES}
-      pageTitle="Carrito de Compras"
-    >
-      <ShoppingCartForm />
-    </DashboardTemplate>
+    <>
+      <DashboardTemplate
+        displaySearchBar={false}
+        sideBarItems={helpers.CLIENT_PAGES}
+        pageTitle="Carrito de Compras"
+      >
+        <ShoppingCartForm setLoading={setLoading} />
+      </DashboardTemplate>
+      {loading ? <Loader loading={loading} /> : <></>}
+    </>
   );
 }
 
