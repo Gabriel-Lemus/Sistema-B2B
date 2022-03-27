3979e6b9cd4d7f6284a186c47587030b728e4df1
Gabriel Lemus <glemus.stuart@gmail.com>
2022-03-27 16:53:56 -0600

    Update style

--- client/src/components/templates/DashboardTemplate.jsx
+++ client/src/components/templates/DashboardTemplate.jsx
@@ -96,7 +96,7 @@ function DashboardTemplate(props) {
             </div>
           </nav>
           <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-            <h1>{props.pageTitle}</h1>
+            <h1 className='mt-3'>{props.pageTitle}</h1>
             {props.children}
           </main>
         </div>
