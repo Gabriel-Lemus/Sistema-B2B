132bf8315f2a80a520487446434f0aed1b8c289a
Gabriel Lemus <glemus.stuart@gmail.com>
2022-03-27 21:35:14 -0600

    Update table columns

--- server/sales-system/src/main/java/SalesServlet.java
+++ server/sales-system/src/main/java/SalesServlet.java
@@ -26,19 +26,19 @@ public class SalesServlet extends HttpServlet {
                 new String[][] {
                     { "id_credencial", "id_cliente", "id_vendedor", "tipo_usuario", "email", "salt", "hash" },
                     { "id_cliente", "nombre", "nit", "email", "telefono", "patente_comercio", "tipo_cliente", "tiene_suscripcion", "vencimiento_suscripcion" },
-                    { "id_vendedor", "nombre" },
+                    { "id_vendedor", "nombre", "es_admin" },
                     { "id_marca", "nombre" },
                 },
                 new String[][] {
                     { "INTEGER", "INTEGER", "INTEGER", "VARCHAR2", "VARCHAR2", "VARCHAR2", "VARCHAR2" },
                     { "INTEGER", "VARCHAR2", "INTEGER", "VARCHAR2", "VARCHAR2", "VARCHAR2", "VARCHAR2", "VARCHAR2", "DATE" },
-                    { "INTEGER", "VARCHAR2" },
+                    { "INTEGER", "VARCHAR2", "VARCHAR2" },
                     { "INTEGER", "VARCHAR2" },
                 },
                 new boolean[][] {
                     { false, true, true, false, false, false, false },
                     { false, false, true, true, true, true, true, false, true },
-                    { false, false },
+                    { false, false, false },
                     { false, false },
                 },
                 new int[] { 100, 100, 100, 100 });
