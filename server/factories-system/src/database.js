94abf45c6803293a313df251bcabe8644e9eb0c5
Gabriel Lemus <glemus.stuart@gmail.com>
2022-04-10 12:55:32 -0600

    Remove old files

--- server/factories-system/src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/db-factory', {
-    //useCreateIndex: true,
-    useNewUrlParser: true
-    //useFindAndModify: false
-})
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.log(err));
