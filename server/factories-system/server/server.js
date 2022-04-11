1c057228fd31a1ed0dee0146b09517451f74c494
Gabriel Lemus <glemus.stuart@gmail.com>
2022-04-10 18:05:51 -0600

    Add initial factories backend implementation

---
+++ server/factories-system/server/server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.FACTORIES_PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Functions
+/**
+ * Attempts to connect to MongoDB.
+ */
+const connectToMongoDB = () => {
+  try {
+    mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+    });
+  } catch (error) {
+    console.log(`Error connecting to MongoDB: ${error}`);
+  }
+};
+
+// Router
+const router = require("./routes/routes");
+
+// Connect to MongoDB
+connectToMongoDB();
+
+// Handle mongoose connection errors
+const db = mongoose.connection;
+db.on("error", (error) => console.log(`Error with MongoDB: ${error}`));
+db.once("open", () => console.log("Connected to MongoDB"));
+
+// App config
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', router);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
+});
