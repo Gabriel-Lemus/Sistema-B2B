7a3a37c846b5cc22e1248c5e45500cc6649a919f
Gabriel Lemus <glemus.stuart@gmail.com>
2022-04-10 22:45:28 -0600

    Enable cors

--- server/factories-system/server/server.js
+++ server/factories-system/server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.FACTORIES_PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -34,7 +35,12 @@ db.once("open", () => console.log("Connected to MongoDB"));
 // App config
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', router);
+app.use("/", router);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Start server
 app.listen(PORT, () => {
