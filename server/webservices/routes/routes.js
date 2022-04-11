9f9b0f83f64718c1ccdfae3de787742f0492609c
Gabriel Lemus <glemus.stuart@gmail.com>
2022-04-11 01:10:06 -0600

    Add webservice to get the seller's data given its email

---
+++ server/webservices/routes/routes.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+const LOCAL_HOST_IP = process.env.LOCAL_HOST_IP;
+const SALES_BE_PORT = process.env.SALES_BE_PORT;
+const FACTORIES_BE_PORT = process.env.FACTORIES_BE_PORT;
+
+// =============================== CRUD Routes ===============================
+// ================================= Create ==================================
+router.post("/", (req, res) => {
+  res.status(400).send({
+    success: false,
+    message: "Please provide the required parameters.",
+  });
+});
+
+// =================================== Read ===================================
+router.get("/", async (req, res) => {
+  const params = req.query;
+
+  // Get the seller's data given its email
+  if (params.sellerEmail !== undefined) {
+    try {
+      const sellerInfo = await axios.get(
+        `http://${LOCAL_HOST_IP}:${SALES_BE_PORT}/sales-system/sales?table=credenciales_usuarios&exists=${params.sellerEmail}`
+      );
+      console.log(sellerInfo.data.data);
+
+      if (sellerInfo.data.success) {
+        res.status(200).send({
+          success: true,
+          message: "Request successful.",
+          data: sellerInfo.data.data !== undefined ? sellerInfo.data.data : [],
+        });
+      } else {
+        res.status(400).send({
+          success: false,
+          message: "The seller email does not exist.",
+        });
+      }
+    } catch (error) {
+      res.status(400).send({
+        success: false,
+        message: `Error getting the seller's data: ${error}`,
+      });
+    }
+  } else {
+    res.status(400).send({
+      success: false,
+      message: "Please provide the required parameters.",
+    });
+  }
+});
+
+// ================================== Update ==================================
+router.put("/", (req, res) => {
+  res.status(400).send({
+    success: false,
+    message: "Please provide the required parameters.",
+  });
+});
+
+// ================================== Delete ==================================
+router.delete("/", async (req, res) => {
+  res.status(400).send({
+    success: false,
+    message: "Please provide the required parameters.",
+  });
+});
+
+module.exports = router;
