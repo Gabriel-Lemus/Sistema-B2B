132a5ae865e979a89edb3cda0152cfde7f47c995
Gabriel Lemus <glemus.stuart@gmail.com>
2022-04-15 01:19:32 -0600

    Change attributes requirements

--- server/factories-system/server/models/orders.js
+++ server/factories-system/server/models/orders.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const OrderSchema = new mongoose.Schema({
   clientId: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
   completed: { type: Boolean, required: true },
-  maxDeliveryDate: { type: Date, required: true },
+  maxDeliveryDate: { type: Date, required: false },
   deliveredDate: { type: Date, required: false },
   canceled: { type: Boolean, required: true },
   completelyPayed: { type: Boolean, required: false },
@@ -13,7 +13,7 @@ const OrderSchema = new mongoose.Schema({
       deviceId: { type: mongoose.Schema.Types.ObjectId, ref: "Device" },
       quantity: { type: Number, required: true },
       price: { type: Number, required: true },
-      estimatedDeliveryDate: { type: Date, required: true },
+      estimatedDeliveryDate: { type: Date, required: false },
       delivered: { type: Boolean, required: true },
       payed: { type: Boolean, required: true },
       deliveredDate: { type: Date, required: false },
