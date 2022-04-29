32c6eddb1f09f67ae45483a20fa0867ae03de9ec
Gabriel Lemus <glemus.stuart@gmail.com>
2022-04-29 16:43:17 -0600

    Update schema attributes

--- server/factories-system/server/models/orders.js
+++ server/factories-system/server/models/orders.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
+  _id: { type: Number, required: true },
   clientId: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
   completed: { type: Boolean, required: true },
   maxDeliveryDate: { type: Date, required: false },
